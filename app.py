b00c7928f2daf21c4786361f36db3e703cb90ec1
rtarquir <rtarquir@unjbg.edu.pe>
2021-06-19 23:31:20 -0500

    Actualizado de datos + Insercion de productos

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import template
 from flask import Flask
 # Referencia a herramientas y librerias
 from flask import render_template, request
@@ -31,6 +32,38 @@ def index():  # def index()<--- nombre de la funcion
     return render_template('inventario/index.html')
 
 # si clonaste esto es porque ya aprendiste a actulizar desde la master
+
+
+@app.route('/insertar_producto')
+def insertar_producto():
+    return render_template('inventario/insertar_producto.html')
+
+
+@app.route('/almacenar_pro', methods=['POST'])
+def almacenarproducto():
+    # recepcion de datos
+    _nombre = request.form['nombre-pro']
+    _marca = request.form['marca-pro']
+    _material = request.form['material-pro']
+    _forma = request.form['forma']
+    _codigo = request.form['codigo-pro']
+    _categoria = request.form['categoria-pro']
+    _stock = request.form['stock-pro']
+    _genero = request.form['genero-pro']
+    _color = request.form['color-pro']
+    _precio = request.form['precio-pro']
+    _foto = request.files['imagen-pro']
+    _uv = request.form['uv-pro']
+ # recepcion de datos
+    sql = "INSERT INTO `Optica`.`PRODUCTO` (`pro_nombre`,`pro_cat_fk`, `pro_stock`, `pro_color`, `pro_material`, `pro_precio`, `pro_marca`, `pro_num`, `pro_proteccionuv`, `pro_forma`, `pro_genero`,`pro_imagen`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    # los parametros segun el orden de datos
+    datos = (_nombre, _categoria, _stock, _color, _material,
+             _precio, _marca, _codigo, _uv, _forma, _genero, _foto.filename)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()  # Commit hace que la conexion se termine
+    return render_template('inventario/index.html')
 
 
 if __name__ == '__main__':  # para empezar la aplicacion
