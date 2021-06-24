fb45573a7db7cb4bfcf2b5d68a1402633b721f46
richardtr123 <richard.alex.t1@gmail.com>
2021-06-24 08:08:01 -0500

    actualizado

--- app.py
+++ app.py
@@ -19,16 +19,6 @@ mysql.init_app(app)
 
 @app.route('/')  # Recibe solicitudes mediante la url
 def index():  # def index()<--- nombre de la funcion
-
-    # TODO: CONEXION BD y QUERIES
-    #sql="consulta, insercion eliminacion, visualizacion, sintaxis mysql"
-    sql = "INSERT INTO `Optica`.`CLIENTE` (`cli_nombre`, `cli_apellido1`, `cli_apellido2`, `cli_correo`, `cli_dni_o_ruc`, `cli_fechanac`, `cli_genero`, `cli_telefono`, `cli_direccion`) VALUES ('Alexander', 'Tarqui', 'Rondon', 'alextr.t1@hotmail.com', '70848524', '2006-06-16', 'Masculino', '938290325', 'Alto Alianza');"
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    conn.commit()  # Commit hace que la conexion se termine
-    # TODO: CONEXION BD y QUERIES
-    # luego de la insercion nos dirige a la siguiente pagina
     return render_template('inventario/index.html')
 
 # si clonaste esto es porque ya aprendiste a actulizar desde la master
