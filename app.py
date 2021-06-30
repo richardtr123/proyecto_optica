34234207d394a9f28c43024637c2b284c531a038
richardtr123 <richard.alex.t1@gmail.com>
2021-06-29 20:58:31 -0500

    Arreglo de bugs

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ def almacenarproducto():
     _genero = ""
     _color = ""
     _uv = ""
-    _material = ""
     _tipoluna = ""
     _tipomaterial = ""
     _tipofiltro = ""
@@ -51,6 +50,7 @@ def almacenarproducto():
     # CONTROL DE categoria
     _categoria = request.form['option']
     if _categoria == '1': #moNTURA
+        _uv = ""
         _genero = request.form['genero-pro']
         _color = request.form['color-pro']
         _calibre = request.form['calibre-pro']
@@ -59,19 +59,21 @@ def almacenarproducto():
         _medida = _calibre+"-"+_puente+"-"+_ancho
     if _categoria == '2': #luna
         _uv = request.form['uv-pro']
-        _material = request.form['material-pro'] 
         _tipoluna = request.form['tipoluna'] 
         _tipomaterial = request.form['tipomaterial'] 
-        if _tipomaterial == '1':
+        if _tipomaterial == 'Organico':
             _tipofiltro = request.form['tipo-filtro-organico'] 
-        if _tipomaterial == '2':
+        if _tipomaterial == 'Vidrio':
             _tipofiltro = request.form['tipo-filtro-vidrio'] 
-        if _tipomaterial == '3':
+        if _tipomaterial == 'Policarbonato':
             _tipofiltro = request.form['tipo-filtro-policarbonato']                        
     if _categoria == '3': #lente de contacto
+        _uv = ""
         _tipocontacto = request.form['tipocontacto'] 
-        if _tipocontacto == '1':
-            _color = request.form['tipocontacto-color']                            
+        if _tipocontacto == 'Cosmetico':
+            _color = request.form['tipocontacto-color']
+    if _categoria == '4':
+        _uv = ""                            
 
     # recepcion de datos        
     _stock = request.form['stock-pro']    
@@ -81,7 +83,7 @@ def almacenarproducto():
  # recepcion de datos
     sql = "INSERT INTO `PRODUCTO` (`pro_cat_fk`, `pro_stock`, `pro_color`, `pro_material`, `pro_precio`, `pro_marca`, `pro_codigo`, `pro_proteccionuv`, `pro_genero`, `pro_nombre`, `pro_imagen`, `pro_tipoluna`, `pro_filtro`, `pro_lentecontacto`,`pro_medida`) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s)"
     # los parametros segun el orden de datos
-    datos = (_categoria,_color,_material,_stock,_precio,_marca,_codigo,_uv,_genero,_nombre,_foto.filename,_tipoluna,_tipofiltro,_tipocontacto,_medida)
+    datos = (_categoria,_stock,_color,_tipomaterial,_precio,_marca,_codigo,_uv,_genero,_nombre,_foto.filename,_tipoluna,_tipofiltro,_tipocontacto,_medida)
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql, datos)
