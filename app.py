d3fc62113c3d5b334489fa4a99f1078007f2a828
richardtr123 <richard.alex.t1@gmail.com>
2021-07-05 21:50:40 -0500

    Paso final para la insercion

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from re import template
-from flask import Flask
+from flask import Flask, url_for, redirect
 # Referencia a herramientas y librerias
 from flask import render_template, request
 from flaskext.mysql import MySQL  # Base del modulo de Mysql
@@ -43,22 +43,28 @@ def almacenarproducto():
     _tipomaterial = ""
     _tipofiltro = ""
     _tipocontacto = ""
+    _forma = ""
+    _nombre = ""
+    _marca = ""
+    _codigo = ""
     # RECEPCION DE CATEGORIA
     _nombre = request.form['nombre-pro']
-    _marca = request.form['marca-pro']
     _codigo = request.form['codigo-pro']
+    
     # CONTROL DE categoria
     _categoria = request.form['option']
     if _categoria == '1': #moNTURA
+        _marca = request.form['marca-pro-montura']
         _uv = ""
         _genero = request.form['genero-pro']
         _color = request.form['color-pro']
         _calibre = request.form['calibre-pro']
         _puente = request.form['puente-pro']
         _ancho = request.form['ancho-pro']
-        _medida = _calibre+"-"+_puente+"-"+_ancho
+        _forma = request.form['forma-montura']
+        _medida = _calibre+"-"+_puente+"-"+_ancho #medida cuando sea una montura
     if _categoria == '2': #luna
-        _uv = request.form['uv-pro']
+        _marca = request.form['marca-pro-luna']
         _tipoluna = request.form['tipoluna'] 
         _tipomaterial = request.form['tipomaterial'] 
         if _tipomaterial == 'Organico':
@@ -68,26 +74,30 @@ def almacenarproducto():
         if _tipomaterial == 'Policarbonato':
             _tipofiltro = request.form['tipo-filtro-policarbonato']                        
     if _categoria == '3': #lente de contacto
+        _marca = request.form['marca-pro-lentecontacto']
         _uv = ""
         _tipocontacto = request.form['tipocontacto'] 
         if _tipocontacto == 'Cosmetico':
             _color = request.form['tipocontacto-color']
+        if _tipocontacto == 'Miopia':
+            _medida = request.form['medida-contacto'] #medida cuando sea un lente de contacto
     if _categoria == '4':
         _uv = ""                            
 
+    
     # recepcion de datos        
     _stock = request.form['stock-pro']    
     _precio = request.form['precio-pro']
     _foto = request.files['imagen-pro']
     
  # recepcion de datos
-    sql = "INSERT INTO `PRODUCTO` (`pro_cat_fk`, `pro_stock`, `pro_color`, `pro_material`, `pro_precio`, `pro_marca`, `pro_codigo`, `pro_proteccionuv`, `pro_genero`, `pro_nombre`, `pro_imagen`, `pro_tipoluna`, `pro_filtro`, `pro_lentecontacto`,`pro_medida`) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s)"
+    sql = "INSERT INTO `PRODUCTO` (`pro_cat_fk`, `pro_stock`, `pro_color`, `pro_material`, `pro_precio`, `pro_marca`, `pro_codigo`, `pro_proteccionuv`, `pro_genero`, `pro_nombre`, `pro_imagen`, `pro_tipoluna`, `pro_filtro`, `pro_lentecontacto`,`pro_medida`,`pro_forma`) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s,%s)"
     # los parametros segun el orden de datos
-    datos = (_categoria,_stock,_color,_tipomaterial,_precio,_marca,_codigo,_uv,_genero,_nombre,_foto.filename,_tipoluna,_tipofiltro,_tipocontacto,_medida)
+    datos = (_categoria,_stock,_color,_tipomaterial,_precio,_marca,_codigo,_uv,_genero,_nombre,_foto.filename,_tipoluna,_tipofiltro,_tipocontacto,_medida,_forma)
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql, datos)
-    conn.commit()  # Commit hace que la conexion se termine
+    conn.commit()  # commit hace que la conexion se termine
     return render_template('inventario/index.html')
 
 
